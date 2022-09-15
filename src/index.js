0310d22e5531baafbf5bd9a59d62c0c9a70e49d6
Monikku12 <monique.paymentboulanger@gmail.com>
2022-09-16 08:17:42 +0900

    deploy Success, Senry installed and functional

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+Sentry.init({
+  dsn: "https://65fe208466c344089e733949214fe366@o1412707.ingest.sentry.io/6752020",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Can also use with React Concurrent Mode
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
 if (process.env.NODE_ENV === "production") {
     disableReactDevTools();
